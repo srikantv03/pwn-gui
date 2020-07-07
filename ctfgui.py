5d250c3c447110e2e3d8505289033a014622d219
Srikant Vasudevan <sv092003@gmail.com>
2020-07-07 13:30:31 -0400

    initial commit (boilerplate code)

---
+++ ctfgui.py
@@ -0,0 +1,63 @@
+from pwn import *
+
+
+hostname = str(input("Enter hostname: "))
+
+if "\n" in hostname:
+	hostname = hostname.split("\n")[0]
+
+for i in range (0, 100):
+	try:
+		port = int(input("Port: "))
+		break
+	except:
+		print("Please enter a number")
+		
+		
+		
+print("Execute code below (return a string to send to the server) ")
+
+caseword = input("Enter an execution key: ")
+
+function_start = "def return_value():"
+
+print("> " + function_start)
+
+execution_str = function_start + "\n"
+while True:
+	current_line = input("> ")
+	
+	if current_line == caseword:
+		print("Executing...")
+		print(execution_str)
+		break
+	execution_str = execution_str + current_line + "\n"
+	
+	
+exec(execution_str)
+print(return_value())
+
+	
+	
+try:
+	r = remote(hostname, port)
+except: 
+	print("Something went wrong! Check your hostname and port number...")
+
+
+try:
+	print(r.recv().decode())
+except:
+	print("Something went wrong")
+
+
+
+r.send(return_value().encode('utf-8'))
+
+try:
+	print(r.recv().decode())
+	r.interactive()
+except:
+	print("Something went wrong")
+	
+print(r.recv().decode())
---
+++ init.sh
@@ -0,0 +1,5 @@
+git clone http://github.com/treecant/pwn-gui
+sudo apt install python3-pip
+pip3 install pwn
+cd pwn-gui
+python3 main.py
